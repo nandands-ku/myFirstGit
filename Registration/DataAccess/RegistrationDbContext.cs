35914c1fb380f1c9ce7fade2d1d1c7e91f6d9232
Nandan Kumar <nandandas.ku@gmail.com>
2020-12-23 12:25:06 +0600

    Add code structure

---
+++ Registration/DataAccess/RegistrationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class RegistrationDbContext : DbContext
+    {
+        public RegistrationDbContext(DbContextOptions<RegistrationDbContext> dbContext) : base(dbContext)
+        {
+
+        }
+
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
